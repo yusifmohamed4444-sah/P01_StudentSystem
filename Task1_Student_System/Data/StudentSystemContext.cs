c77065d210238cfcc8251a06ee475b79c54873d0
Youssef Mohamed <yusif.mohamed4444@gmail.com>
2026-07-18 22:27:38 +0300

    Add relationships and seed data

--- Task1_Student_System/Data/StudentSystemContext.cs
+++ Task1_Student_System/Data/StudentSystemContext.cs
@@ -69,6 +69,123 @@ namespace P01_StudentSystem.Data
                 .WithMany(S => S.Homeworks)
                 .HasForeignKey(H => H.StudentId);
 
+
+            modelBuilder.Entity<Student>().HasData(
+
+                new Student
+                {
+                    StudentId = 1,
+                    Name = "Youssef",
+                    PhoneNumber = "1012750565",
+                    RegisteredOn = new DateTime(2026, 7, 18),
+                    Birthday = new DateTime(2002, 4, 13)
+                },
+
+                new Student
+                {
+                    StudentId = 2,
+                    Name = "Mohamed",
+                    PhoneNumber = "1281736381",
+                    RegisteredOn = new DateTime(2026, 7, 19),
+                    Birthday = new DateTime(2002, 4, 13)
+                }
+
+            );
+
+
+            modelBuilder.Entity<Course>().HasData(
+
+                new Course
+                {
+                    CourseId = 1,
+                    Name = "C#",
+                    Description = "Introduction to C#",
+                    StartDate = new DateTime(2026, 7, 20),
+                    EndDate = new DateTime(2026, 10, 13),
+                    Price = 1500
+                },
+
+                new Course
+                {
+                    CourseId = 2,
+                    Name = "C++",
+                    Description = "Introduction to C++",
+                    StartDate = new DateTime(2026, 7, 20),
+                    EndDate = new DateTime(2026, 10, 13),
+                    Price = 1500
+                }
+            );
+
+            modelBuilder.Entity<StudentCourse>().HasData(
+
+                  new StudentCourse
+                  {
+                      StudentId = 1,
+                      CourseId = 1
+                  },
+
+                  new StudentCourse
+                  {
+                      StudentId = 1,
+                      CourseId = 2
+                  },
+
+                  new StudentCourse
+                  {
+                      StudentId = 2,
+                      CourseId = 1
+                  }
+
+            );
+
+            modelBuilder.Entity<Resource>().HasData(
+
+               new Resource
+               {
+                   ResourceId = 1,
+                   Name = "Introduction",
+                   Url = "https://example.com/csharp",
+                   ResourceType = ResourceType.Video,
+                   CourseId = 1
+               },
+
+               new Resource
+               {
+                   ResourceId = 2,
+                   Name = "SQL Notes",
+                   Url = "https://example.com/sql",
+                   ResourceType = ResourceType.Document,
+                   CourseId = 2
+               }
+            );
+
+
+            modelBuilder.Entity<Homework>().HasData(
+
+              new Homework
+              {
+                  HomeworkId = 1,
+                  Content = "Homework 1",
+                  ContentType = ContentType.Pdf,
+                  SubmissionTime = new DateTime(2026, 7, 20),
+                  StudentId = 1,
+                  CourseId = 1
+              },
+
+                 new Homework
+                 {
+                     HomeworkId = 2,
+                     Content = "Homework 2",
+                     ContentType = ContentType.Zip,
+                     SubmissionTime = new DateTime(2026, 7, 25),
+                     StudentId = 2,
+                     CourseId = 1
+                 }
+            );
+
+
+
+
         }
     }
 }
---
+++ Task1_Student_System/Migrations/20260718191615_SeedData.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace P01_StudentSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "CourseId", "Description", "EndDate", "Name", "Price", "StartDate" },
+                values: new object[,]
+                {
+                    { 1, "Introduction to C#", new DateTime(2026, 10, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "C#", 1500m, new DateTime(2026, 7, 20, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "Introduction to C++", new DateTime(2026, 10, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "C++", 1500m, new DateTime(2026, 7, 20, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "Birthday", "Name", "PhoneNumber", "RegisteredOn" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2002, 4, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "Youssef", "1012750565", new DateTime(2026, 7, 18, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, new DateTime(2002, 4, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "Mohamed", "1281736381", new DateTime(2026, 7, 19, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Homeworks",
+                columns: new[] { "HomeworkId", "Content", "ContentType", "CourseId", "StudentId", "SubmissionTime" },
+                values: new object[,]
+                {
+                    { 1, "Homework 1", 1, 1, 1, new DateTime(2026, 7, 20, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "Homework 2", 2, 1, 2, new DateTime(2026, 7, 25, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Resources",
+                columns: new[] { "ResourceId", "CourseId", "Name", "ResourceType", "Url" },
+                values: new object[,]
+                {
+                    { 1, 1, "Introduction", 0, "https://example.com/csharp" },
+                    { 2, 2, "SQL Notes", 2, "https://example.com/sql" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "StudentCourses",
+                columns: new[] { "CourseId", "StudentId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 1 },
+                    { 1, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Homeworks",
+                keyColumn: "HomeworkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Homeworks",
+                keyColumn: "HomeworkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Resources",
+                keyColumn: "ResourceId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Resources",
+                keyColumn: "ResourceId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumns: new[] { "CourseId", "StudentId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumns: new[] { "CourseId", "StudentId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumns: new[] { "CourseId", "StudentId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 2);
+        }
+    }
+}
